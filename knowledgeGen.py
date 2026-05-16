5e2469f071fa59dcf23a12aab7a62fdf21adc9ae
Andrés Ospina <andres.felipe.ospina.cardenas@stud.uni-hannover.de>
2026-05-16 18:27:56 +0200

    checked

--- knowledgeGen.py
+++ knowledgeGen.py
@@ -12,8 +12,8 @@ client = OpenAI(api_key= os.getenv("OPENAI_API_KEY"))
 
 def create_knowledge(text, culture, dimension):
     """
+    Extracts important features and content related to a specific culture
     from a given text. Returns format: title, original snippet and knowledge extrated from the text.
-    from a given text. Returns format: title, original snippet, paragraph if possible.
     Does not invent information if there is insufficient support.
     """
     if isinstance(text, str):
