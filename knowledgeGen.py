5385c54d69d4f69184c81c863d1a73c1cf7827b0
Andrés Ospina <andres.felipe.ospina.cardenas@stud.uni-hannover.de>
2026-07-16 21:29:54 +0200

    OPENAI_Knowledge_creation moved

--- knowledgeGen.py
+++ knowledgeGen.py
@@ -11,49 +11,6 @@ def translate(text, target_lang):
     return GoogleTranslator(source="en", target=target_lang).translate(text)
 
 
-def OpenAI_create_knowledge(text, culture, dimension):
-    """
-    Extracts important features and content related to a specific culture using OpenAI API
-    from a given text. Returns format: title, original snippet and knowledge extrated from
-    the text. Does not invent information if there is insufficient support.
-    """
-    if isinstance(text, str):
-        texts = [text]
-    else:
-        texts = text
-
-    prompt_texts = "\n\n".join([f"Text {i}:\n{text}" for i, text in enumerate(texts, 1)])
-
-
-    prompt = f"""
-    You are an expert assistant in cultural text analysis.
-    Your task is to read the following texts provided and extract only the relevant information related to the {culture} culture.
-    Just return one single result for all text provided and return result in a structured format with the following fields:
-
-    - title: a short title summarizing the feature.
-    - culture": {culture},
-    - dimension": {dimension},
-    - snippet: the original text supporting this feature. If there are multiple texts, you put one snippet for each text, separated by //.
-    - Knowledge: a concise summary of the information related to the {dimension} dimension in the {culture} culture, based solely on the provided text.
-
-    If the text **does not contain enough information about the culture**, do not invent anything and say "Not enough information." in the knowledge field, but fill the title with the dimension and culture and added a (info missing), so it can be tracked.
-
-    Texts to analyze:
-    {prompt_texts}
-
-    """
-    load_dotenv()
-
-    client = OpenAI(api_key= os.getenv("OPENAI_API_KEY"))
-
-    response = client.chat.completions.create(
-        model="gpt-4.1-mini",
-        messages=[{"role": "user", "content": prompt}]
-    )
-
-    content = response.choices[0].message.content
-    return content
-
 
 def knowledge_level_manager(args, timestamp, query_results):
     """
